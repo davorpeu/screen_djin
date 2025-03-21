8ae4a8f09ee4bb25267cb3eb53400de2cef897f5
Davor Peuraca <davorpeu@gmail.com~>
2025-03-21 15:05:17 +0100

    vite setup

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {AppRoutes} from '@/routes';
 
 export const Router: React.FC = () => {
     return (
-        <BrowserRouter basename="/screen_djin">
+         <HashRouter basename="/screen_djin">
             <AppRoutes />
-        </BrowserRouter>
+         </HashRouter>
     );
 };
