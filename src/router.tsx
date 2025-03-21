f5a2bc4f9a7dbb90bc619bd1daed7a9763fc615a
Davor Peuraca <davorpeu@gmail.com~>
2025-03-21 13:50:41 +0100

    bulletproof react refactor, lists finished.

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { AppRoutes } from './routes/index';
+import {BrowserRouter} from 'react-router-dom';
+import {AppRoutes} from '@/routes';
 
 export const Router: React.FC = () => {
     return (
